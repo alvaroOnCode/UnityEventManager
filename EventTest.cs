46f3abf3142f5cc732cac9787f317ff772c2899b
Álvaro Saavedra <alvarojsaavedra@gmail.com>
2016-12-20 17:31:01 +0100

    Completing example with two more scripts.

---
+++ EventTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventTest : MonoBehaviour {
+
+	//##################################################
+	//	UNITY FUNCTIONS
+	//##################################################
+	void OnEnable ()
+	{
+		EventManager.StartListening ("test", SomeFunction);
+		EventManager.StartListening (EventManager.SPAWN, SomeOtherFunction);
+		EventManager.StartListening (EventManager.DESTROY, SomeThirdFunction);
+	}
+
+	void OnDisable ()
+	{
+		EventManager.StopListening ("test", SomeFunction);
+		EventManager.StopListening (EventManager.SPAWN, SomeOtherFunction);
+		EventManager.StopListening (EventManager.DESTROY, SomeThirdFunction);
+	}
+
+
+	//##################################################
+	//	ALVARO FUNCTIONS
+	//##################################################
+	void SomeFunction ()
+	{
+		Debug.Log ("SomeFunction was called!");
+	}
+
+	void SomeOtherFunction ()
+	{
+		Debug.Log ("SomeOtherFunction was called!");
+	}
+
+	void SomeThirdFunction ()
+	{
+		Debug.Log ("SomeThirdFunction was called!");
+	}
+
+}
---
+++ EventTriggerTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTriggerTest : MonoBehaviour {
+
+	//##################################################
+	//	UNITY FUNCTIONS
+	//##################################################
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("t")) {
+			EventManager.TriggerEvent ("test");
+		}
+
+		if (Input.GetKeyDown ("s")) {
+			EventManager.TriggerEvent (EventManager.SPAWN);
+		}
+
+		if (Input.GetKeyDown ("d")) {
+			EventManager.TriggerEvent (EventManager.DESTROY);
+		}
+	}
+
+}
